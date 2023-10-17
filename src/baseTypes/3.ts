c3d1f4ea8f5c47b74a154b1de292a1b2ccf4a81f
Yevhenii Lukashov <115802889+EuJinnLucaShow@users.noreply.github.com>
2023-10-17 17:30:03 +0300

    Summary

--- src/baseTypes/3.ts
+++ src/baseTypes/3.ts
@@ -1,10 +1,6 @@
 let some: unknown;
 some = 'Text';
 let str: string;
-if (typeof some === 'string') {
-  str = some;
-} else {
-  // Обробка випадку, коли some не є рядком.
-}
+if (typeof some === 'string') str = some;
 
 export {};
